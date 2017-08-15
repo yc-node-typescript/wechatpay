f240b04b5d287ef11aeabb1990629a60e207aeb9
hcy123123 <1968730948@qq.com>
2017-08-15 19:20:24 +0800

    微信支付查看订单，退款

--- __tests__/wechatpay-spec.ts
+++ __tests__/wechatpay-spec.ts
@@ -1,4 +1,4 @@
-import { IConfig, IOrderParams, Wechatpay } from '../src/wechatpay';
+import { IConfig, IOrder , IOrderParams, Wechatpay } from '../src/wechatpay';
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 
@@ -9,24 +9,26 @@ test('Should greet with message', async () => {
     apiKey: '80Adxl69yfEMzlZ5WSAnG4jAtXs5yITm', //微信商户平台API密钥
     notify_url: `http://example.com`,
     trade_type: 'APP', //APP, JSAPI, NATIVE etc.
-    pfx: readFileSync(resolve('./apiclient_cert.p12'))
+    pfx: readFileSync(resolve('./apiclient_cert.p12')),
   };
   const wechatpay = new Wechatpay(config);
   const orderParams: IOrderParams = {
-    body: '支付测试', // 商品或支付单简要描述 
-    out_trade_no: 'order1', // 商户系统内部的订单号,32个字符内、可包含字母 
+    body: '支付测试', // 商品或支付单简要描述
+    out_trade_no: 'order1', // 商户系统内部的订单号,32个字符内、可包含字母
     total_fee: 100,
     spbill_create_ip: '192.168.2.1',
     notify_url: 'http://wxpayment_notify_url',
-    trade_type: 'APP'
+    trade_type: 'APP',
+  };
+  const IOrders :IOrder ={
+       out_trade_no: 'order1'
   };
   const appOrder = await wechatpay.createUnifiedOrderForApp(orderParams);
-  console.log(appOrder);
   expect(appOrder).toMatchObject({
     partnerid: expect.stringMatching(/\d{10}/),
     prepayid: expect.stringMatching(/[a-z0-9]{36}/),
     noncestr: expect.stringMatching(/.+/),
     timestamp: expect.stringMatching(/\d{10}/),
-    sign: expect.stringMatching(/[A-Z0-9]{32}/)
+    sign: expect.stringMatching(/[A-Z0-9]{32}/),
   });
 });
--- src/wechatpay.ts
+++ src/wechatpay.ts
@@ -33,6 +33,21 @@ export interface IOrderResult {
   trade_type: 'JSAPI' | 'APP' | 'NATIVE';
 }
 
+export interface IOrder {
+  out_trade_no: string;
+}
+
+export interface IcloseResult {
+  return_code: 'SUCCESS' | 'FAIL';
+  return_msg: string;
+  appid: string;
+  mch_id: string;
+  nonce_str: string;
+  sign: string;
+  result_code: string;
+}
+
+
 export interface IAPPOrderResult {
   partnerid: string; // merchant id
   prepayid: string; // prepay id
@@ -41,12 +56,73 @@ export interface IAPPOrderResult {
   sign: string; // signed string
 }
 
+export interface Iquertorder {
+  return_code: 'SUCCESS' | 'FAIL';
+  return_msg: string;
+  appid: string;
+  mch_id: string; // 'your merchant id',
+  nonce_str: 'P6IFNTlKWVKtlOH4';
+  sign: string;
+  result_code: string;
+  out_trade_no: string;
+  trade_state: string;
+  trade_state_desc: string;
+}
+
+export interface IRefund {
+  return_code: 'SUCCESS' | 'FAIL';
+  return_msg: string;
+  appid: string;
+  mch_id: string;
+  nonce_str: string;
+  sign: string;
+  result_code: string;
+  transaction_id: string;
+  out_trade_no: string;
+  out_refund_no: string;
+  refund_id: string;
+  refund_channel: string;
+  refund_fee: string;
+  coupon_refund_fee: string;
+  total_fee: string;
+  cash_fee: string;
+  coupon_refund_count: string;
+  cash_refund_fee: string;
+}
+
+export interface IQueryRefund {
+  appid: string;
+  cash_fee: string;
+  mch_id: string;
+  nonce_str: string;
+  out_refund_no_0: string;
+  out_trade_no: string;
+  refund_channel_0: string;
+  refund_count: string;
+  refund_fee: string;
+  refund_fee_0: string;
+  refund_id_0: string;
+  refund_recv_accout_0: string;
+  refund_status_0: string;
+  result_code: string;
+  return_code: string;
+  return_msg: string;
+  sign: string;
+  total_fee: string;
+  transaction_id: string;
+}
+
 export interface IWechatpay {
   signVerify: (xml: any) => Promise<boolean>;
   success: () => string;
   fail: () => string;
   createUnifiedOrder: (obj: IOrderParams) => Promise<IOrderResult>;
   createUnifiedOrderForApp: (obj: IOrderParams) => Promise<IAPPOrderResult>;
+  queryorder: (obj: IOrder) => Promise<Iquertorder>; //查询订单
+  closeorder: (obj: IOrder) => Promise<IcloseResult>; //关闭订单
+  Refundorder: (obj: IOrder) => Promise<IRefund>;
+  QueryRefund: (obj: IOrder) => Promise<IQueryRefund>;
+
 }
 
 export class Wechatpay extends WechatPayment implements IWechatpay {
@@ -61,9 +137,26 @@ export class Wechatpay extends WechatPayment implements IWechatpay {
     return this.__config;
   }
 
+  public Refundorder = (obj: IOrder): Promise<IRefund> => {
+    return super.refund(obj);
+  };
+
+  public QueryRefund = (obj: IOrder): Promise<IQueryRefund> => {
+    return super.queryRefund(obj);
+  }
+
+
   public createUnifiedOrder = (obj: IOrderParams): Promise<IOrderResult> => {
     return super.createUnifiedOrder(obj);
-  }
+  };
+
+  public queryorder = (obj: IOrder): Promise<Iquertorder> => {
+    return super.queryOrder(obj);
+  };
+
+  public closeorder = (obj: IOrder): Promise<IcloseResult> => {
+    return super.closeOrder(obj);
+  };
 
   public signVerify = async notification => {
     let dataForSign = Object.assign({}, notification);
@@ -72,9 +165,11 @@ export class Wechatpay extends WechatPayment implements IWechatpay {
     return signValue === notification.sign;
   };
 
-  public createUnifiedOrderForApp = async (obj: IOrderParams): Promise<IAPPOrderResult> => {
+  public createUnifiedOrderForApp = async (
+    obj: IOrderParams
+  ): Promise<IAPPOrderResult> => {
     let res = await this.createUnifiedOrder(obj);
-    if(res.return_code !== 'SUCCESS') throw new Error(res.return_msg);
+    if (res.return_code !== 'SUCCESS') throw new Error(res.return_msg);
     let params: any = {
       appid: this.config.appid,
       partnerid: res.mch_id,
@@ -83,17 +178,18 @@ export class Wechatpay extends WechatPayment implements IWechatpay {
       noncestr: res.nonce_str,
       timestamp: String(Math.floor(new Date().getTime() / 1000)),
     };
-    let str = Object.keys(params).sort()
-      .map(k => `${k}=${params[k]}`)
-      .join('&') + '&key=' + this.config.apiKey;
+    let str =
+      Object.keys(params).sort().map(k => `${k}=${params[k]}`).join('&') +
+      '&key=' +
+      this.config.apiKey;
     params.sign = createHash('md5').update(str).digest('hex').toUpperCase();
     return params;
-  }
+  };
 
   public success(): string {
     return WechatPaymentUtils.buildXML({ xml: { return_code: 'SUCCESS' } });
   }
-  
+
   public fail(): string {
     return WechatPaymentUtils.buildXML({ xml: { return_code: 'FAIL' } });
   }
